f629c28f7c45305f65e18f694710e7e33fdc5e77
Rennan Prysthon <rennan.prysthon@liferay.com>
2022-01-28 23:32:49 -0300

    feat: mark and unmark checked items

--- js/main.js
+++ js/main.js
@@ -4,10 +4,57 @@ const resetButton = document.querySelector('#reset-btn');
 
 let tabSize = 4;
 
+function clearChecked() {
+  let allChecked = document.querySelectorAll('#tab > div.checked') || []
+  allChecked.forEach(el => {
+    el.classList.remove('checked');
+  })
+}
+
+function drawLines() {
+  clearChecked()
+
+  let allSelected = document.querySelectorAll('#tab > div.selected') || []
+  
+  allSelected.forEach(el => {
+    let [posY, posX] = el.id.substr(1).split('-').map(val => Number(val))
+
+    let elements = []
+  
+    for (let i = 0; i < tabSize; i++) {
+      let query = `#p${posY}-${i}`;
+      let element = document.querySelector(query)
+      elements.push(element)
+    }
+  
+    for (let i = 0; i < tabSize; i++) {
+      let query = `#p${i}-${posX}`;
+      let element = document.querySelector(query)
+      elements.push(element)
+    }
+  
+    elements.forEach(ele =>{
+      if (!ele.classList.contains(`checked`)) {
+        ele.classList.add(`checked`);
+      }
+    })
+  })
+}
+
 function onClickTabItem(event) {
   let { target } = event
   
+  if (
+    target.classList.contains('checked') &&
+    !target.classList.contains('selected')
+  ) {
+    return;
+  }
+  
   target.classList.toggle('selected')
+
+  drawLines()
+  target.classList.remove('checked')
 }
 
 function drawTab(size = tabSize) {
@@ -17,6 +64,7 @@ function drawTab(size = tabSize) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       let ele = document.createElement('div')
+      ele.id = `p${i}-${j}`;
       ele.onclick = onClickTabItem
 
       tab.appendChild(ele)
